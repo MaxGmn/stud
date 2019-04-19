14976959e6457e082ba9c109b52ee6ae2c6db78d
Максим Гуменюк <maksym_humeniuk@epam.com>
2019-04-19 13:04:03 +0300

    first task is done

--- ArrayDictionarySet/ArrayDictionarySet/ViewController.swift
+++ ArrayDictionarySet/ArrayDictionarySet/ViewController.swift
@@ -13,8 +13,26 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        firstTask();
     }
-
-
 }
+
+func firstTask(){
+    
+    let monday: Set = ["stud01", "stud02", "stud05", "stud06", "stud07"]
+    let tuesday: Set = ["stud03", "stud06", "stud08", "stud10"]
+    let wednesday: Set = ["stud01", "stud03", "stud07", "stud09", "stud10"]
+    
+    print("Students, who were all days: \(monday.intersection(tuesday).intersection(wednesday).sorted())\n")
+    
+    print("Students, who were just two days:\n" +
+        "\t monday and tueday: \(monday.intersection(tuesday).sorted())\n" +
+        "\t tuesday and wednesday: \(tuesday.intersection(wednesday).sorted())\n" +
+        "\t monday and wednesday: \(monday.intersection(wednesday).sorted())\n")
+    
+    print("Students, who were in monday and wednestay, but not tuesday: \(monday.intersection(wednesday).subtracting(tuesday).sorted())")
+}
+
+
+
 
