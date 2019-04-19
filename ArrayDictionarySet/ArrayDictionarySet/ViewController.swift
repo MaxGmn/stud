5a75f18cf54148ff0bc4edf14a01f6666f3f6098
Максим Гуменюк <maksym_humeniuk@epam.com>
2019-04-19 11:49:05 +0300

    ArrayDictionarySet first commit

---
+++ ArrayDictionarySet/ArrayDictionarySet/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  ArrayDictionarySet
+//
+//  Created by Maksym Humeniuk on 4/19/19.
+//  Copyright © 2019 Maksym Humeniuk. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
