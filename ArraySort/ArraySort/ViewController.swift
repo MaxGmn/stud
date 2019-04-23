ab24996378aa6cbbd1351155a517fb379d45d9db
Максим Гуменюк <maksym_humeniuk@epam.com>
2019-04-23 10:43:53 +0300

    added func bubbleSort

--- ArraySort/ArraySort/ViewController.swift
+++ ArraySort/ArraySort/ViewController.swift
@@ -13,8 +13,32 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        
+        let testArray = [9, 8, 7, 6, 5, 4, 3, 2, 1]
+        
+        print(bubbleSort(inputArray: testArray))
     }
 
 
+    func bubbleSort(inputArray:[Int]) -> [Int]{
+        var sortedArray = inputArray
+        if sortedArray.count > 1{
+            
+            for _ in 0..<sortedArray.count - 1{
+                for j in 1..<sortedArray.count{
+                    if sortedArray[j] < sortedArray[j-1]{
+                        let temp = sortedArray[j]
+                        sortedArray[j] = sortedArray[j-1]
+                        sortedArray[j-1] = temp
+                    }
+                }
+            }
+        }
+        return sortedArray;
+    }
+    
+    func inputSort(inputArray:[Int]) -> [Int] {
+        <#function body#>
+    }
 }
 
