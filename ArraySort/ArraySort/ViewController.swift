54faf4461380f85c7ae4864827a511f68eb00eec
Максим Гуменюк <maksym_humeniuk@epam.com>
2019-04-24 11:16:23 +0300

    added quickSort

--- ArraySort/ArraySort/ViewController.swift
+++ ArraySort/ArraySort/ViewController.swift
@@ -17,8 +17,10 @@ class ViewController: UIViewController {
         let testArray = [9, 8, 7, 6, 5, 4, 3, 2, 1]
         let testArray2 = [5, 3, 7, 1, 8, 5, 4, 3, 2, 1]
         
-        print(bubbleSort(inputArray: testArray))
-        print(inputSort(inputArray: testArray2))
+//        print(bubbleSort(inputArray: testArray))
+//        print(inputSort(inputArray: testArray2))
+        
+         print(quickSort(inputArray: testArray))
     }
 
 
@@ -52,6 +54,44 @@ class ViewController: UIViewController {
                 }
                 sortedArray[j+1] = key
             }
+        }
+        return sortedArray;
+    }
+    
+    func quickSort(inputArray:[Int]) -> [Int] {
+        
+        var tempArray = inputArray
+        
+        var i = 0
+        var j = tempArray.count - 1
+        let p = tempArray[tempArray.count/2]
+        
+        while i < j {
+            while tempArray[i] < p { i += 1 }
+            while tempArray[j] > p { j -= 1 }
+            
+            if i < j {
+                let temp = tempArray[i]
+                tempArray[i] = tempArray[j]
+                tempArray[j] = temp
+                i += 1
+                j -= 1
+            }
+        }
+        
+        var frontSide = [Int]()
+        var backSide = [Int]()
+        if j > 0 { frontSide = quickSort(inputArray: [Int](tempArray[0...j-1]))}
+        if tempArray.count - 1 > i { backSide = quickSort(inputArray: [Int](tempArray[i+1...tempArray.count - 1]))}
+        
+        return frontSide + [p] + backSide
+    }
+    
+    func mergeSort(inputArray:[Int]) -> [Int] {
+        var sortedArray = inputArray
+        
+        if sortedArray.count > 1{
+
         }
         return sortedArray;
     }
