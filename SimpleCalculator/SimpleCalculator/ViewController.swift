6a8805cf91908c45168e33172aafc87bab0274c2
Максим Гуменюк <maksym_humeniuk@epam.com>
2019-04-26 10:06:37 +0300

    initial commit

---
+++ SimpleCalculator/SimpleCalculator/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  SimpleCalculator
+//
+//  Created by Maksym Humeniuk on 4/26/19.
+//  Copyright © 2019 Maksym Humeniuk. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
