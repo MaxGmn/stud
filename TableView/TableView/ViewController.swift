36db054d1f17145c2d6aa0e0e9b568a3c702b023
Максим Гуменюк <maksym_humeniuk@epam.com>
2019-04-30 11:10:35 +0300

    table created and filled

--- TableView/TableView/ViewController.swift
+++ TableView/TableView/ViewController.swift
@@ -9,12 +9,42 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
+    @IBOutlet weak var mainTable: UITableView!
+    
+    let dataStructures = ["Stack", "Queue", "Array", "Set", "Dictionary", "HashTable"]
+    
+    
+ 
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
 
+    
 
+}
+
+extension ViewController: UITableViewDataSource{
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return dataStructures.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let currentCell = tableView.dequeueReusableCell(withIdentifier: "cellID", for: indexPath)
+        
+        currentCell.textLabel?.text = dataStructures[indexPath.row]
+        
+        return currentCell
+    }
+}
+
+extension ViewController: UITableViewDelegate{
+    
+    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath){
+        tableView.deselectRow(at: indexPath, animated: true)
+    }
 }
 
