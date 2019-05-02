ae72646538eae2276fe24eee4a2ef5d2f3c8adf2
Максим Гуменюк <maksym_humeniuk@epam.com>
2019-05-02 13:51:12 +0300

    part one is done

--- ArraySortDemonstration/ArraySortDemonstration/ViewController.swift
+++ ArraySortDemonstration/ArraySortDemonstration/ViewController.swift
@@ -10,11 +10,46 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var tableView: UITableView!
+    
+    @IBOutlet weak var sortTypeSwitcher: UISegmentedControl!
+    
+    var array = arrayFilling()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+//        array = arrayFilling()
         // Do any additional setup after loading the view.
     }
 
+    @IBAction func nextOnAction(_ sender: Any) {
+        
+        let sortingResult = sortTypeSwitcher.selectedSegmentIndex == 0 ? bubbleSort(inputArray: array) : inputSort(inputArray: array)
+        
+        array = sortingResult.sortedArray
+        
+        let fromIndexPath = IndexPath(row: sortingResult.from, section: 0)
+        let toIndexPath = IndexPath(row: sortingResult.to, section: 0)
+        tableView.moveRow(at: fromIndexPath, to: toIndexPath)       
+    }    
+}
 
+extension ViewController: UITableViewDataSource{
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return array.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let currentCell = tableView.dequeueReusableCell(withIdentifier: "cellID", for: indexPath)
+        currentCell.textLabel?.text = "\(array[indexPath.row])"
+        
+        return currentCell
+    }   
+}
+
+extension ViewController: UITableViewDelegate{
+    
 }
 
