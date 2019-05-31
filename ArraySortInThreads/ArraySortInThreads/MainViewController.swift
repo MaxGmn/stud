f9b7d39d8ed53dff21fee9a5b86010f3dfff3334
Максим Гуменюк <maksym_humeniuk@epam.com>
2019-05-31 15:58:09 +0300

    sorting is transferred to the queue

--- ArraySortInThreads/ArraySortInThreads/MainViewController.swift
+++ ArraySortInThreads/ArraySortInThreads/MainViewController.swift
@@ -1,72 +1,84 @@
 //
-//  MainTableViewController.swift
+//  MainViewController.swift
 //  ArraySortInThreads
 //
-//  Created by Maksym Humeniuk on 5/30/19.
+//  Created by Maksym Humeniuk on 5/31/19.
 //  Copyright © 2019 Maksym Humeniuk. All rights reserved.
 //
 
 import UIKit
 
-class MainTableViewController: UITableViewController {
-    
+class MainViewController: UIViewController {
+
     private var sectionsArray = [SortTypes]()
-    private var rowsArray = [[String]]()    
+    private var rowsArray = [[String]]()
     private var inputData = [SortTypes : [BaseArrayType]]()
     private let numberOfIterations = 50
     private var rowsCount: Float = 0
     private var rowsDone: Float = 0
+    private let operationQueue = OperationQueue()
     
-    @IBOutlet weak var progressBar: UIProgressView!
-    
+    @IBOutlet private weak var progressBar: UIProgressView!
+    @IBOutlet private weak var tableView: UITableView!
     
     @IBAction func startAction(_ sender: Any) {
-        rowsDone = 0
         for (sectionNumber, sortType) in sectionsArray.enumerated() {
             guard let arrayTypes = inputData[sortType] else {return}
             for (rowNumber, arrayType) in arrayTypes.enumerated() {
-                let resultString = ArrayManager.getResultArraySorting(sortType: sortType, arrayType: arrayType, numberOfIterations)
-                rowsArray[sectionNumber][rowNumber] = resultString
-                let indexPath = IndexPath(row: rowNumber, section: sectionNumber)
-                tableView.reloadRows(at: [indexPath], with: .automatic)
-                rowsDone += 1
-                progressBar.setProgress(rowsDone/rowsCount, animated: true)
+                operationQueue.addOperation {
+                    let resultString = ArrayManager.getResultArraySorting(sortType: sortType, arrayType: arrayType, self.numberOfIterations)
+                    self.rowsArray[sectionNumber][rowNumber] = resultString
+                    let indexPath = IndexPath(row: rowNumber, section: sectionNumber)
+                    DispatchQueue.main.async {
+                        self.tableView.reloadRows(at: [indexPath], with: .automatic)
+                        self.rowsDone += 1
+                        self.progressBar.setProgress(self.rowsDone/self.rowsCount, animated: true)
+                    }
+                }
             }
         }
     }
-
+    
+    @IBAction func cancelAction(_ sender: Any) {
+        operationQueue.cancelAllOperations()
+    }    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         inputData = fillDataDictionary()
         createEmptyTable()
+        operationQueue.qualityOfService = .utility
     }
 
-    // MARK: - Table view data source
-    override func numberOfSections(in tableView: UITableView) -> Int {
+}
+
+extension MainViewController: UITableViewDataSource, UITableViewDelegate {    
+    
+    func numberOfSections(in tableView: UITableView) -> Int {
         return sectionsArray.count
     }
-
-    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
         return rowsArray[section].count
     }
-
-    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         let cell = tableView.dequeueReusableCell(withIdentifier: "MainCell", for: indexPath)
         cell.textLabel?.text = rowsArray[indexPath.section][indexPath.row]
         return cell
     }
-
-    override func tableView(_ tableView: UITableView, titleForHeaderInSection section: Int) -> String? {
+    
+    func tableView(_ tableView: UITableView, titleForHeaderInSection section: Int) -> String? {
         return sectionsArray[section].rawValue
     }
 }
 
-private extension MainTableViewController {
+private extension MainViewController {
     
     func createEmptyTable() {
         sectionsArray = inputData.keys.compactMap({$0})
         for section in sectionsArray {
-            let tmpArray = Array(repeating: "in process", count: inputData[section]?.count ?? 0)
+            let tmpArray = Array(repeating: "no data", count: inputData[section]?.count ?? 0)
             rowsCount += Float(tmpArray.count)
             rowsArray.append(tmpArray)
         }
