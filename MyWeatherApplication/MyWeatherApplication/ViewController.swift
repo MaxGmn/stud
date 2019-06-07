955b4ab4cef3b34d5d5a460d3045d0491b1254bf
Максим Гуменюк <maksym_humeniuk@epam.com>
2019-06-07 15:20:35 +0300

    initial commit

---
+++ MyWeatherApplication/MyWeatherApplication/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  MyWeatherApplication
+//
+//  Created by Maksym Humeniuk on 6/7/19.
+//  Copyright © 2019 Maksym Humeniuk. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
